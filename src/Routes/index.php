d31adb8e4a2e8362c21930b9cbabfe6bb73a15c7
solomon-mh <solomon.muhye.wd@gmail.com>
2025-08-03 01:35:12 +0300

    Add index.php for routing and request handling in TaskManager API

---
+++ src/Routes/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Solomon\TaskManagerApiPhp\Controllers\TaskController;
+use Solomon\TaskManagerApiPhp\Database\Database;
+
+header('Content-Type: application/json');
+
+$db = Database::getInstance()->getConnection();
+$controller = new TaskController($db);
+
+$requestUri = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (isset($requestUri[0]) && $requestUri[0] === 'tasks') {
+    $id = $requestUri[1] ?? null;
+    $controller->handleRequest($method, $id);
+} else {
+    http_response_code(404);
+    echo json_encode(['error' => 'Not Found']);
+}
